42c16691c8102efc429a30b0cb2f5a9254ea2086
Sarah Blankenship <smblank@clemson.edu>
2024-07-08 21:11:47 -0400

    Started Interface implementations

---
+++ app/src/main/java/com/example/bettersweepy/Chore.java
@@ -0,0 +1,19 @@
+package com.example.bettersweepy;
+
+public class Chore implements IChore{
+    String name;
+    int effortVal;
+    int time;
+    int frequency;
+    IChore deepClean;
+
+    @Override
+    public void editChore() {
+
+    }
+
+    @Override
+    public void printChore() {
+
+    }
+}
